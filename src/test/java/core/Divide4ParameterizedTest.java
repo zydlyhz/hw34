0072465125cda75d6797a971967ef14b13fca7e7
Jeeka Tursunbai <zt9074@gmail.com>
2019-06-30 13:33:02 -0700

    hw34push

---
+++ src/test/java/core/Divide4ParameterizedTest.java
@@ -0,0 +1,52 @@
+package core;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+
+
+public class Divide4ParameterizedTest {
+	
+	@Parameters(name = "Parameter # {index}: + {1} + {2} = {0}")
+	
+	public static Collection<Double[]>divideNumbers() {
+		
+		return Arrays.asList(new Double[][] {
+			
+			{3.0,1.0,2.0},  // { 0.5, 1.0, 2.0 }, 
+			{5.0,2.0,3.0},  // { 0.66, 2.0, 3.0 }, 
+			{7.0,3.0,4.0}, //  { 0.75, 3.0, 4.0 }, 
+			{9.0,4.0,5.0} //  { 0.8, 4.0, 5.0 },
+		});
+	}
+	
+	@Parameter(value = 0) 
+	public Double expected;
+	
+	@Parameter(value = 1)
+	public Double first;
+	
+	@Parameter(value = 2) 
+	public Double second;
+	
+	
+	@Test
+	
+	public void multiply() {
+		System.out.println(first + " + " + second + " + " + expected);
+		
+		assertEquals("Not correct", Calculator.divide(first, second), expected, 0.09);
+		
+	}
+	
+
+}
+
+
